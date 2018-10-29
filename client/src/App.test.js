b4bd73e1899987980a6cd5c351a6f90197bcc629
Anne LoVerso <aelv13@gmail.com>
2018-10-29 15:00:36 -0400

    Add eslint

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import App from './App';
 import { mount } from 'enzyme';
+import App from './App';
 
 describe('<App />', () => {
   it('displays stations from the station store', () => {
     const component = mount(<App />);
-    expect(component.find('[data-station]').first().text()).toContain('Grand Central')
+    expect(component.find('[data-station]').first().text()).toContain('Grand Central');
   });
 });
