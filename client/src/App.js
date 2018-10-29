6b2a687bd922fbd8bddb64265902ba0ddad97b8b
Anne LoVerso <aelv13@gmail.com>
2018-10-29 14:47:20 -0400

    Add enzyme and setup tests

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+      <div>
 
         <div data-station>Grand Central - 42 St</div>
         <div data-station>Canal St</div>
